52a381b21eb707d73ebd673fa17ddc78eab07d86
kafkiansky <vadimzanfir@gmail.com>
2025-12-11 13:31:12 +0300

    remove afterLoad

--- _config.ts
+++ _config.ts
@@ -9,30 +9,4 @@ const site = lume({
 site.use(plugins());
 site.use(date());
 
-site.addEventListener("afterLoad", () => {
-    const posts = site.pages.filter((p) => p.data.url?.includes("/posts/") && p.data.date);
-
-    const groups: Record<string, any[]> = {};
-    for (const post of posts) {
-        const d = post.data.date instanceof Date ? post.data.date : new Date(post.data.date);
-        const year = String(d.getFullYear());
-
-        const data = {
-            ...post.data,
-            date: d,
-        };
-
-        (groups[year] ??= []).push(data);
-    }
-
-    for (const year of Object.keys(groups)) {
-        groups[year].sort((a, b) => b.date.getTime() - a.date.getTime());
-    }
-
-    const years = Object.keys(groups).map(Number).sort((a, b) => b - a).map(String);
-
-    site.data("years", years);
-    site.data("posts", groups);
-});
-
 export default site;
